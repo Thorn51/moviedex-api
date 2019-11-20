8e0f96f9b6987e8f7b942b4b1ec65496093fe8ac
Matt Thornberry <mtberry16@hotmail.com>
2019-11-19 19:44:50 -0700

    Strapped on the helmet

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
 const cors = require("cors");
 const MOVIES = require("./movies.json");
 const app = express();
 
 app.use(morgan("common"));
+app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
