2bb3f22da5c1f7bc0cf36ee785048891f489b198
Matt Thornberry <mtberry16@hotmail.com>
2019-11-19 19:09:23 -0700

    Validate bearer token

--- server.js
+++ server.js
@@ -4,10 +4,21 @@ const morgan = require("morgan");
 const MOVIES = require("./movies.json");
 const app = express();
 
+console.log(process.env.API_TOKEN);
+
 app.use(morgan("common"));
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
 app.get("/movie", function handleGetMovies(req, res) {
-  res.json(MOVIES);
+  res.send("Movie Search API");
 });
 
 app.listen(8000, () => {
